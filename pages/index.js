234e6bbd62520c7dce1e818a35b9e1bc695aa7ce
vgeruso <victor.geruso@gmail.com>
2022-06-24 16:40:18 -0300

    [ADD] Elements Home page

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,47 @@
-import styles from "../styles/Home.module.css";
+import Image from "next/image";
 
-export default function Home() {
-  return <h1>PokeNext</h1>;
+import Card from "../components/Card";
+
+import Styles from "../styles/Home.module.css";
+
+export async function getStaticProps() {
+  const maxPokemons = 252;
+  const api = "https://pokeapi.co/api/v2/pokemon/";
+
+  const res = await fetch(`${api}/?limit=${maxPokemons}`);
+  const data = await res.json();
+
+  // add pokemon index
+  data.results.forEach((item, index) => {
+    item.id = index + 1;
+  });
+
+  return {
+    props: {
+      pokemons: data.results,
+    },
+  };
+}
+
+export default function Home({ pokemons }) {
+  return (
+    <>
+      <div className={Styles.title_container}>
+        <h1 className={Styles.title}>
+          Poke<span>Next</span>
+        </h1>
+        <Image
+          src="/images/pokeball.png"
+          width="50"
+          height="50"
+          alt="PokeNext"
+        />
+      </div>
+      <div className={Styles.pokemon_container}>
+        {pokemons.map((pokemon) => (
+          <Card key={pokemon.id} pokemon={pokemon} />
+        ))}
+      </div>
+    </>
+  );
 }
